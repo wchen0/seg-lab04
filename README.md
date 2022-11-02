156a2e5128db48be58282089f8e3cdc52dfabb0e
201180009-Chen Wei Tian <201180009@smail.nju.edu.cn>
2022-11-02 14:45:03 +0800

    add readme

---
+++ README.md
@@ -0,0 +1,3 @@
+### NJUCS, Software Engineering, Lab04
+
+Generate random input for multiple C++ programs and judge whether they are equivalent.
---
+++ eq.h
@@ -0,0 +1,8 @@
+#ifndef __EQ__
+#define __EQ__
+
+void generate_input(const string &, const string &);
+
+bool checkIdentical(const string &, const string &);
+
+#endif
